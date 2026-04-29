de4de291e4c39dec16f93f35ac192ebcd902c91f
Patrick Alves <cpatrickalves@gmail.com>
2026-04-29 10:11:24 -0300

    Update install-plugins.sh

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -1,6 +1,6 @@
-claude plugin install feature-dev@claude-plugins-official --scope project
+# Essentials 
+claude plugin install github@claude-plugins-official
 claude plugin install claude-md-management@claude-plugins-official --scope project
-claude plugin install pr-review-toolkit@claude-plugins-official --scope project
 claude plugin install skill-creator -s project
 
 # Python
